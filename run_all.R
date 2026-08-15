f34b84a272350e867c97d90bf386726d90560ac0
silviaa6 <152346213+silviaa6@users.noreply.github.com>
2026-08-15 17:10:20 -0400

    Update run_all.R

--- run_all.R
+++ run_all.R
@@ -1,6 +1,6 @@
 #!/usr/bin/env Rscript
 # Run from repository root.
 source(file.path("R","reproduce_manuscript.R"))
-source(file.path("R","make_variance_figure.R"))
-source(file.path("R","make_longitudinal_figure.R"))
+source(file.path("R","make_figure4_variance.R"))
+source(file.path("R","make_figure5_longitudinal.R"))
 cat("All analyses and figure scripts completed.\n")
